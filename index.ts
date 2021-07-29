53ecb02e6ad77f056eb8b47ae6b770591d42ea4c
Maxime <maxime.lalo.pro@gmail.com>
2021-07-29 20:48:25 +0200

    Cors ntm

--- index.ts
+++ index.ts
@@ -2,10 +2,18 @@ import {config} from "dotenv";
 config();
 import express from "express";
 import {buildRoutes} from "./routes";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = ['*'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
 
 buildRoutes(app);
 
