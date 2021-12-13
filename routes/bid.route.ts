1e42b5f95dde27ab7b97425ac0c37947d57b6a2b
Maxime Lalo <maxime.lalo.pro@gmail.com>
2021-12-13 15:15:16 +0100

    Update bid.route.ts

--- routes/bid.route.ts
+++ routes/bid.route.ts
@@ -4,7 +4,7 @@ import {BidController} from "../controllers";
 
 const router = express.Router();
 
-router.get("/", async function(req, res) 
+router.get("/", async function(req, res)  {
     const connection = await DatabaseUtils.getConnection();
     const bidController = new BidController(connection);
     const bidsList = await bidController.getAll();
