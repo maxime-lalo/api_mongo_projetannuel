500de72aa0a202073becb6211793329d3b8ed2a2
Maxime <maxime.lalo.pro@gmail.com>
2021-12-13 16:18:20 +0100

    Cors update

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['*'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
