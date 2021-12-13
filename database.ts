5426384bb56e996b944c26e2eae241fc5173fd98
Maxime Lalo <maxime.lalo.pro@gmail.com>
2021-12-13 14:44:51 +0100

    Update database.ts

--- database.ts
+++ database.ts
@@ -3,19 +3,21 @@ config();
 import { MongoClient } from "mongodb";
 
 export class DatabaseUtils {
-    try{
         private static connection?: MongoClient;
 
         static async getConnection(): Promise<MongoClient> {
+            
+            try{
+                
             let uri:string = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&writeConcern=majority`;
             const client = new MongoClient(uri);
             await client.connect();
             const database = client.db(process.env.DB_NAME);
 
             return client;
+            }catch(err){
+                console.log(err);
+            }
         }
-    }catch(err){
-        console.log(err)
-    }
 
 }
